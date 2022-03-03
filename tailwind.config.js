2dd18481ea8724a021d50876b9858a18f925ab24
philopian <pdotwdot@gmail.com>
2022-03-02 19:28:21 -0800

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
